bb584d7bf26f7afe9ac8645a6aa854ebfd448e6a
hanzalaareeb <22f1000707@student.onlinedegree.iitm.ac.in>
2025-01-06 02:11:56 +0530

    don't know the problem

--- django_project/accounts/admin.py
+++ django_project/accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    change = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        "email",
+        "username",
+        "name",
+        "is_staff",
+    ]
+    fieldsets = UserAdmin.fieldsets + (
+        None, {"fields": ("name",)},
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        None, {"fields": ("name",)},
+    )
